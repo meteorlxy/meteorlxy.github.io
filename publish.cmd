4ac951b95403173c6c4fb20461579a6be8a96f36
Xinyu Liu <meteor.lxy@foxmail.com>
2018-05-31 12:03:26 +0800

    workflow: setup travis ci

--- publish.cmd
+++
@@ -1,8 +0,0 @@
-cd dist
-echo www.meteorlxy.cn > CNAME
-git init
-git remote add origin https://github.com/meteorlxy/meteorlxy.github.io.git
-git add .
-git commit -m "update"
-git push origin master --force
-cd ..
