5a54ca3bb291745b8be27a51e24ad2ac7fbd4046
Xinyu Liu <meteor.lxy@foxmail.com>
2019-01-30 23:44:51 +0800

    chore: bump vuepress and theme version

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,15 +1,20 @@
 module.exports = {
   title: 'Meteorlxy',
+
   description: 'meteorlxy\'s homepage',
+
   head: [
     ['link', { rel: 'icon', href: '/assets/img/avatar.jpg' }],
   ],
+
   locales: {
     '/': {
       lang: 'zh-CN',
     },
   },
+
   theme: 'meteorlxy',
+
   themeConfig: {
     personalInfo: {
       nickname: 'meteorlxy',
@@ -17,50 +22,64 @@ module.exports = {
       email: 'meteor.lxy@foxmail.com',
       location: 'Xi\'an City, China',
       organization: 'Xi\'an Jiao Tong University',
+
       avatar: '/assets/img/avatar.jpg',
+
       sns: {
-        facebook: {
-          account: 'meteorlxy.cn',
-          link: 'https://www.facebook.com/meteorlxy.cn'
-        },
         github: {
           account: 'meteorlxy',
-          link: 'https://github.com/meteorlxy'
+          link: 'https://github.com/meteorlxy',
+        },
+        facebook: {
+          account: 'meteorlxy.cn',
+          link: 'https://www.facebook.com/meteorlxy.cn',
         },
         linkedin: {
           account: 'meteorlxy',
-          link: 'http://www.linkedin.com/in/meteorlxy'
+          link: 'http://www.linkedin.com/in/meteorlxy',
         },
         twitter: {
           account: 'meteorlxy_cn',
-          link: 'https://twitter.com/meteorlxy_cn'
+          link: 'https://twitter.com/meteorlxy_cn',
         },
-        // zhihu: {
-        //   account: 'meteorlxy.cn',
-        //   link: 'https://www.zhihu.com/people/meteorlxy.cn'
-        // },
-        // douban: {
-        //   account: '159342708',
-        //   link: 'https://www.zhihu.com/people/159342708'
-        // },
         weibo: {
           account: '@焦炭君_Meteor',
-          link: 'https://weibo.com/u/2039655434'
-        }
-      }
+          link: 'https://weibo.com/u/2039655434',
+        },
+        zhihu: {
+          account: 'meteorlxy.cn',
+          link: 'https://www.zhihu.com/people/meteorlxy.cn',
+        },
+        douban: {
+          account: '159342708',
+          link: 'https://www.douban.com/people/159342708',
+        },
+      },
     },
-    // headerBackground priority: url > useGeo
+
     headerBackground: {
       // url: '/assets/img/bg.jpg',
-      useGeo: true
+
+      useGeo: true,
     },
-    lastUpdated: true,
+
     nav: [
       { text: 'Home', link: '/', exact: true },
       { text: 'Posts', link: '/posts/', exact: false  },
       { text: 'About', link: '/about/', exact: false  }, 
-    ]
+    ],
+
+    comments: {
+      platform: 'github',
+      owner: 'meteorlxy',
+      repo: 'meteorlxy.github.io',
+      clientId: '960bbc7e80512b06153e',
+      clientSecret: 'd63ad26efd7e29fc5a9533742d7fc5c88347b6ba',
+      prefix: '[Post Comments] ',
+      labels: ['comments'],
+    },
   },
+
   markdown: {
     toc: {
       includeLevel: [2, 3, 4]
